f80c46b902987b204ef44322120120c170193e26
Hillary Hoyt <hoyt.hillary@gmail.com>
2016-08-31 19:01:58 -0400

    changed hello text back

--- src/hillary/scratch/HelloWorldRunner.java
+++ src/hillary/scratch/HelloWorldRunner.java
@@ -2,6 +2,6 @@ package hillary.scratch;
 
 public class HelloWorldRunner {
 	public static void main(String[] args) {
-		System.out.println("Bonjour, le monde!");
+		System.out.println("Hello, World!");
 	}
 }
